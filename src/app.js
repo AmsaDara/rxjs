289514b41d9af6a49ad62afdb8d8984aa58a0383
amsadara <thiiba95@gmail.com>
2022-02-14 12:13:44 +0000

    animation canvas avec remplissage du souris

--- src/app.js
+++ src/app.js
@@ -1,31 +1,32 @@
-import {fromEvent} from 'rxjs';
-import { pipe } from 'rxjs';
-import { map } from 'rxjs';
-const source$ = fromEvent(document,'click');
+import { fromEvent } from 'rxjs';
+import { pip } from 'rxjs';
+import { map } from 'rxjs'
+
+// get The Canvas node
+const canvas=document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+const radius = 12
 
 
+
+
+const source$ = fromEvent(document, 'mousemove');
 
 source$.pipe(
     map(
-        pointerEvent=>{
-    return ({
-        x:pointerEvent.x,
-        y:pointerEvent.y
-    });}),
-    map(point =>{
-        return ({
-            x:- point.x,
-            y: point.y
-        })
-})).subscribe({
-    next:(data) =>{
-        console.log(data);
-    }
-    // ,
-    // error:(error)=>{
-    //     console.log(error);
-    // },
-    // complete:()=>{
-    //     console.log('complete');
-    // }
-})
+        pointerEvent => {
+            return ({
+                x: pointerEvent.clientX,
+                y: pointerEvent.clientY
+            });
+        }
+    )).subscribe({
+        next: point =>{
+            console.log(point)
+            ctx.beginPath();
+            ctx.fillStyle = 'tomato';
+            ctx.arc(point.x, point.y, radius, 0 * Math.PI, 2 * Math.PI);
+            ctx.fill();
+           
+        }
+    })
